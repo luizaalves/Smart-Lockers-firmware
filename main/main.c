98f862743b4f08c2aeae2cf08622e2f9df4c81f1
Luiza <l.uhzinha@hotmail.com>
2024-06-20 15:59:58 -0300

    Test srv_comm usability

--- main/main.c
+++ main/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "esp_log.h"
+#include "srv_communication.h"
 
 static const char TAG[] = "main";
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "HELLO WORLD");
+    srv_comm_init();
 }
