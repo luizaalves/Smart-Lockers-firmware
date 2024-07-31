7a4d807d9102780d4d0c682bef8859927f9e0416
Luiza <l.uhzinha@hotmail.com>
2024-07-31 14:31:19 -0300

    update nvs functions

--- components/drv/inc/drv_nvs.h
+++ components/drv/inc/drv_nvs.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <stddef.h>
+#include "stdint.h"
 
 #define WIFI_SSID_KEY "wifi_ssid"
 #define WIFI_PASS_KEY "wifi_password"
+#define LOCKER_NAME_KEY "locker_name"
+#define LOCKER_NUM_KEY "locker_num"
 
 typedef enum
 {
@@ -15,9 +18,13 @@ typedef enum
 
 drv_nvs_err_et drv_nvs_init(void);
 
-drv_nvs_err_et drv_nvs_get(const char *namespace_name, const char *key, char *out_value, size_t *length);
+drv_nvs_err_et drv_nvs_get_str(const char *namespace_name, const char *key, char *out_value, size_t *length);
 
-drv_nvs_err_et drv_nvs_set(const char *namespace_name, const char *key, const char *value);
+drv_nvs_err_et drv_nvs_set_str(const char *namespace_name, const char *key, const char *value);
+
+drv_nvs_err_et drv_nvs_get_u8(const char *namespace_name, const char *key, uint8_t *out_value);
+
+drv_nvs_err_et drv_nvs_set_u8(const char *namespace_name, const char *key, uint8_t value);
 
 drv_nvs_err_et drv_nvs_erase_partition(const char *namespace_name);
 
--- components/drv/src/drv_nvs.c
+++ components/drv/src/drv_nvs.c
@@ -16,7 +16,7 @@ drv_nvs_err_et drv_nvs_init(void)
     return DRV_NVS_OK;
 }
 
-drv_nvs_err_et drv_nvs_get(const char *namespace_name, const char *key, char *out_value, size_t *length)
+drv_nvs_err_et drv_nvs_get_str(const char *namespace_name, const char *key, char *out_value, size_t *length)
 {
     nvs_handle_t my_handle;
     esp_err_t err = nvs_open(namespace_name, NVS_READWRITE, &my_handle);
@@ -41,11 +41,49 @@ drv_nvs_err_et drv_nvs_get(const char *namespace_name, const char *key, char *ou
     return DRV_NVS_OK;
 }
 
-drv_nvs_err_et drv_nvs_set(const char *namespace_name, const char *key, const char *value)
+drv_nvs_err_et drv_nvs_get_u8(const char *namespace_name, const char *key, uint8_t *out_value)
+{
+    nvs_handle_t my_handle;
+    esp_err_t err = nvs_open(namespace_name, NVS_READWRITE, &my_handle);
+    if (err != ESP_OK) 
+    {
+        ESP_LOGE(TAG, "Error (%s) opening NVS handle!", esp_err_to_name(err));
+        return DRV_NVS_ERR_EMPTY;
+    }  
+    err = nvs_get_u8(my_handle, key, out_value);
+    if (err == ESP_ERR_NVS_NOT_FOUND) 
+    {
+        ESP_LOGW(TAG, "key not found in NVS");
+        return DRV_NVS_KEY_NOT_FOUND;
+    } 
+    else if (err != ESP_OK) 
+    {
+        ESP_LOGE(TAG, "Error (%s) reading key from NVS!", esp_err_to_name(err));
+        return DRV_NVS_ERR;
+    }
+    nvs_close(my_handle);
+    return DRV_NVS_OK;
+}
+
+drv_nvs_err_et drv_nvs_set_str(const char *namespace_name, const char *key, const char *value)
 {
     nvs_handle_t my_handle;
     esp_err_t err = nvs_open(namespace_name, NVS_READWRITE, &my_handle);
     err = nvs_set_str(my_handle, key, value);
+    if(err != ESP_OK) return DRV_NVS_ERR;
+    printf("Committing updates in NVS ... ");
+    err = nvs_commit(my_handle);
+    if(err != ESP_OK) return DRV_NVS_ERR;
+    printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+    nvs_close(my_handle);
+    return DRV_NVS_OK;
+}
+
+drv_nvs_err_et drv_nvs_set_u8(const char *namespace_name, const char *key, uint8_t value)
+{
+    nvs_handle_t my_handle;
+    esp_err_t err = nvs_open(namespace_name, NVS_READWRITE, &my_handle);
+    err = nvs_set_u8(my_handle, key, value);
     if(err != ESP_OK) return DRV_NVS_ERR;
     printf("Committing updates in NVS ... ");
     err = nvs_commit(my_handle);
