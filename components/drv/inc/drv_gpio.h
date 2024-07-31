9f34dfab691b4233c28ce1faf65e125315ca571a
Luiza <l.uhzinha@hotmail.com>
2024-07-31 14:31:00 -0300

    update gpio functions

--- components/drv/inc/drv_gpio.h
+++ components/drv/inc/drv_gpio.h
@@ -8,7 +8,7 @@ typedef struct
     uint8_t gpio_mode;
     uint8_t gpio_intr_type;
     void *handler;
-}drv_gpio_config_st;
+} drv_gpio_config_st;
 
 typedef enum
 {
@@ -17,6 +17,17 @@ typedef enum
     DRV_GPIO_ERR_INTR_TYPE,
     DRV_GPIO_ERR_ISR_SERVICE,
     DRV_GPIO_ERR_HANDLER,
+    DRV_GPIO_ERR_INVALID_NUM,
 } drv_gpio_err_et;
 
-drv_gpio_err_et drv_gpio_config(drv_gpio_config_st *config);
+drv_gpio_err_et drv_gpio_set_direction(uint8_t gpio_num, uint8_t gpio_mode);
+
+drv_gpio_err_et drv_gpio_set_intr_type(uint8_t gpio_num, uint8_t gpio_intr_type);
+
+drv_gpio_err_et drv_gpio_isr_handler_add(uint8_t gpio_num, void * handler);
+
+drv_gpio_err_et drv_gpio_set_level(uint8_t num, uint32_t on_off);
+
+uint8_t drv_gpio_get_level(uint8_t num);
+
+drv_gpio_err_et drv_gpio_set_pull(uint8_t num);
--- components/drv/src/drv_gpio.c
+++ components/drv/src/drv_gpio.c
@@ -1,14 +1,43 @@
 #include "drv_gpio.h"
 
-drv_gpio_err_et drv_gpio_config(drv_gpio_config_st *config)
+drv_gpio_err_et drv_gpio_set_direction(uint8_t gpio_num, uint8_t gpio_mode)
 {
-    esp_err_t err = gpio_set_direction(config->gpio_num, config->gpio_mode);
+    esp_err_t err = gpio_set_direction(gpio_num, gpio_mode);
     if(err != ESP_OK) return DRV_GPIO_ERR_DIRECTION;
-    err = gpio_set_intr_type(config->gpio_num, config->gpio_intr_type);
+    if(gpio_mode == GPIO_MODE_INPUT) gpio_set_pull_mode(gpio_num, GPIO_PULLUP_ONLY);
+    return DRV_GPIO_OK;
+}
+
+drv_gpio_err_et drv_gpio_set_intr_type(uint8_t gpio_num, uint8_t gpio_intr_type)
+{
+    esp_err_t err = gpio_set_intr_type(gpio_num, gpio_intr_type);
     if(err != ESP_OK) return DRV_GPIO_ERR_INTR_TYPE;
     err = gpio_install_isr_service(0);
     if(err != ESP_OK) return DRV_GPIO_ERR_ISR_SERVICE;
-    err = gpio_isr_handler_add(config->gpio_num, config->handler, NULL);
+    return DRV_GPIO_OK;
+}
+
+drv_gpio_err_et drv_gpio_isr_handler_add(uint8_t gpio_num, void * handler)
+{
+    esp_err_t err = gpio_isr_handler_add(gpio_num, handler, (void * ) gpio_num);
     if(err != ESP_OK) return DRV_GPIO_ERR_HANDLER;
+    return DRV_GPIO_OK;
+}
+
+drv_gpio_err_et drv_gpio_set_level(uint8_t num, uint32_t on_off)
+{
+    drv_gpio_err_et err = gpio_set_level(num, on_off) == ESP_OK ? DRV_GPIO_OK: DRV_GPIO_ERR_INVALID_NUM;
+    return err;
+}
+
+uint8_t drv_gpio_get_level(uint8_t num)
+{
+    return gpio_get_level(num);
+}
+
+drv_gpio_err_et drv_gpio_set_pull(uint8_t num)
+{
+    gpio_set_pull_mode(num, GPIO_PULLDOWN_ENABLE);
+    gpio_set_pull_mode(num, GPIO_PULLUP_DISABLE);
     return DRV_GPIO_OK;
 }
